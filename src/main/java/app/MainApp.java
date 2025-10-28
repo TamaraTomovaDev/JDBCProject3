3166e4e28aff8eb062ec351f26a1a5b566f5882b
TamaraTomovaDev <toma_tomova@hotmail.com>
2025-10-28 11:02:21 +0100

    Update MainApp.java

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -4,13 +4,45 @@ import model.Account;
 import model.UserDetail;
 import repository.AccountRepository;
 import repository.UserDetailRepository;
+import service.AccountService;
+import service.UserDetailService;
+
+import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-        Account a = new Account("jan", "1234");
-        System.out.println(a);
+        // Repositories en services aanmaken
+        AccountRepository accountRepository = new AccountRepository();
+        UserDetailRepository userDetailRepository = new UserDetailRepository();
+        AccountService accountService = new AccountService(accountRepository);
+        UserDetailService userDetailService = new UserDetailService(userDetailRepository);
 
-        new AccountRepository().addAccount(new Account("piet", "geheim"));
+        // === ACCOUNT CRUD ===
+        System.out.println("=== ACCOUNT TEST ===");
+        accountService.createAccount(new Account("jan", "1234"));
+        accountService.createAccount(new Account("piet", "geheim"));
 
+        Account jan = accountService.readAccount("jan");
+        System.out.println("Gelezen account: " + jan.getUsername());
+
+        accountService.updatePassword("jan", "nieuw1234");
+        accountService.delete("piet");
+
+
+// === USERDETAIL CRUD ===
+        System.out.println("\n=== USERDETAIL TEST ===");
+        UserDetail janDetail = new UserDetail("Jansen", "Jan", "jan@example.com", jan);
+        userDetailService.createUserDetail(janDetail);
+
+        UserDetail gelezen = userDetailService.readUserDetail("jan");
+        System.out.println("Gelezen gebruiker: " + gelezen.getFirstName() + " " + gelezen.getLastName());
+
+        userDetailService.updateEmail("jan", "jan.nieuw@example.com");
+
+        List<UserDetail> alleGebruikers = userDetailService.readAllUserDetails();
+        System.out.println("Aantal gebruikers in DB: " + alleGebruikers.size());
+
+        userDetailService.deleteUserDetail("jan");
     }
 }
+
